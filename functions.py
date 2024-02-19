d66f3a5d3dc5f75e60bc43a200ca7ca88d8e402c
Habiba Mohsen <101303283+Habiba-Mohsen@users.noreply.github.com>
2024-02-19 22:49:05 +0200

    final version

---
+++ functions.py
@@ -0,0 +1,144 @@
+
+from PyQt5.QtWidgets import QLabel
+from PyQt5.QtGui import QPixmap, QImage
+import numpy as np
+import image
+from PyQt5.QtGui import QPixmap, QImage
+import cv2
+from PyQt5.QtWidgets import QGraphicsScene, QGraphicsPixmapItem, QGraphicsRectItem
+from PyQt5.QtGui import QPen, QBrush
+from PyQt5.QtCore import QRectF
+from PyQt5.QtGui import QPainterPath
+from PyQt5.QtGui import QImage, QPixmap, QPainter, QBrush, QColor
+from PyQt5.QtCore import Qt, QRectF, QPointF
+from PyQt5.QtWidgets import QGraphicsRectItem, QGraphicsPixmapItem, QGraphicsScene, QGraphicsView, QGraphicsSceneMouseEvent
+
+def plot_fourier_component(real_part, imaginary_part, magnitude_spectrum, phase_spectrum ,label, index,width=0,heigth=0):
+    try:
+        if index == 0:
+            q_img =convert_component_to_qimage(real_part)
+            q_pixmap = QGraphicsPixmapItem(QPixmap.fromImage(q_img))
+            scene = QGraphicsScene()
+            scene.addItem(q_pixmap)
+            label.setScene(scene)
+            label.fitInView(q_pixmap, Qt.KeepAspectRatio)
+            return scene,q_img,real_part
+            # addResizableRectangle(scene,q_img,real_part,width,heigth)
+
+        elif index == 1:
+            q_img = convert_component_to_qimage(imaginary_part)
+            q_pixmap = QGraphicsPixmapItem(QPixmap.fromImage(q_img))
+            scene = QGraphicsScene()
+            scene.addItem(q_pixmap)
+            label.setScene(scene)
+            label.fitInView(q_pixmap, Qt.KeepAspectRatio)
+            return scene,q_img,imaginary_part
+            # addResizableRectangle(scene, q_img,imaginary_part,width,heigth)
+
+        elif index == 3:
+            q_img = convert_component_to_qimage(phase_spectrum)
+            q_pixmap = QGraphicsPixmapItem(QPixmap.fromImage(q_img))
+            scene = QGraphicsScene()
+            scene.addItem(q_pixmap)
+            label.setScene(scene)
+            label.fitInView(q_pixmap, Qt.KeepAspectRatio)
+            return scene,q_img,phase_spectrum
+            # addResizableRectangle(scene, q_img,phase_spectrum,width,heigth)
+
+        elif index==2:
+            # Ensure magnitude_spectrum is not modified in-place
+            magnitude_spectrum_plotted = np.multiply(np.log10(1 + magnitude_spectrum), 20)
+
+                # Convert NumPy array to QPixmap for display
+            q_img = convert_component_to_qimage(magnitude_spectrum_plotted)
+            q_pixmap = QGraphicsPixmapItem(QPixmap.fromImage(q_img))
+            scene = QGraphicsScene()
+            scene.addItem(q_pixmap)
+            label.setScene(scene)
+            label.fitInView(q_pixmap, Qt.KeepAspectRatio)
+            return scene,q_img,magnitude_spectrum
+            # addResizableRectangle(scene, q_img,magnitude_spectrum,width,heigth)
+
+    except Exception as e:
+        print(f"Error in displaying image: {e}")
+
+
+def convert_component_to_qimage(component):
+    # Normalize the component values to the range [0, 255] and convert to uint8
+    component = (component / np.max(component) * 255).astype(np.uint8)
+    height, width = component.shape
+    # Calculate the number of bytes per line in the image
+    bytes_per_line = component.shape[1]
+
+    # Create a QImage from the component data
+    q_image = QImage(component.data.tobytes(), width, height, bytes_per_line, QImage.Format_Grayscale8)
+
+    #     return q_pixmap
+
+    # Return the QImage object
+    return q_image
+
+def addResizableRectangle(scene,image_component,w,h,inner_rect):
+    width = image_component.width()*(w)*0.01
+    height = image_component.height()*(h)*0.01
+    rect_item = QGraphicsRectItem(0, 0, width, height)
+
+    rect_center_x = (image_component.width() - rect_item.rect().width()) / 2
+    rect_center_y = (image_component.height() - rect_item.rect().height()) / 2
+    rect_item = QGraphicsRectItem(0, 0, width, height)
+    rect_item.setPen(QPen(Qt.yellow))  # Set pen color to yellow
+    brush = QBrush(QColor(255, 255, 0, 50))
+    rect_item.setBrush(brush)
+    rect_item.setPos(rect_center_x, rect_center_y)
+    scene.addItem(rect_item)
+    if not inner_rect.isChecked():
+        brush = QBrush(QColor(0, 0, 0, 50))
+        rect_item.setBrush(brush)
+        rect_item.setPos(rect_center_x, rect_center_y)
+        scene.addItem(rect_item)
+        overlay_path = QPainterPath()
+        overlay_path.addRect(0, 0, image_component.width(), image_component.height())
+        rect_path = QPainterPath()
+        rect_path.addRect(rect_center_x, rect_center_y, width, height)
+        # Subtract rect_path from overlay_path
+        overlay_path -= rect_path
+        # Create a QGraphicsPathItem for the resulting path (difference)
+        difference_item = scene.addPath(overlay_path, QPen(Qt.NoPen), QBrush(QColor(255, 255, 0, 50)))
+    return rect_item
+
+
+def ExtractRegion(rect_item, image_component, image_component_array, inner_region_selected):
+    image_component = QGraphicsPixmapItem(QPixmap.fromImage(image_component))
+    rect_scene_pos = rect_item.scenePos()
+    rect_scene_rect = rect_item.rect()
+    rect_image_pos = image_component.mapFromScene(rect_scene_pos)
+    rect_image_rect = image_component.mapFromScene(rect_scene_rect).boundingRect()
+
+    if inner_region_selected.isChecked():
+        mask = np.zeros_like(image_component_array)
+        mask[int(rect_image_pos.y()):int(rect_image_pos.y() + rect_image_rect.height()),
+             int(rect_image_pos.x()):int(rect_image_pos.x() + rect_image_rect.width())] = 1
+    else:
+        mask = np.ones_like(image_component_array)
+        mask[int(rect_image_pos.y()):int(rect_image_pos.y() + rect_image_rect.height()),
+        int(rect_image_pos.x()):int(rect_image_pos.x() + rect_image_rect.width())] = 0
+
+    image_component_array = image_component_array * mask
+    return image_component_array
+
+
+def display_output_image(label, mixed_image):
+    # Convert NumPy array to QImage
+    height, width = mixed_image.shape
+    bytes_per_line = width
+    q_image = QImage(mixed_image.data.tobytes(), width, height, bytes_per_line, QImage.Format_Grayscale8)
+
+    # Display the mixed image in the QGraphicsView
+    scene = QGraphicsScene()
+    pixmap_item = QGraphicsPixmapItem(QPixmap.fromImage(q_image))
+    scene.addItem(pixmap_item)
+
+    label.setScene(scene)
+    label.fitInView(pixmap_item, Qt.KeepAspectRatio)
+
+
---
+++ image.py
@@ -0,0 +1,114 @@
+from PyQt5.QtGui import QPixmap, QImage
+import cv2
+from matplotlib import pylab
+import numpy as np
+import matplotlib as plt
+from PyQt5.QtWidgets import QGraphicsScene, QGraphicsPixmapItem, QGraphicsRectItem
+from PyQt5.QtGui import QPen, QBrush
+from PyQt5.QtCore import QRectF
+from PyQt5.QtGui import QPainterPath
+from PyQt5.QtGui import QImage, QPixmap, QPainter, QBrush, QColor
+from PyQt5.QtCore import Qt, QRectF, QPointF
+from PyQt5.QtWidgets import QGraphicsRectItem, QGraphicsPixmapItem, QGraphicsScene, QGraphicsView, QGraphicsSceneMouseEvent
+
+
+class Image:
+
+    def __init__(self, image_path, width, height):
+        self.image_path = image_path
+        self.width = width
+        self.height = height
+        self.image = self.read_image()
+        self.components = self.calculate_img_magnitude_phase(self.image)
+
+    def read_image(self):
+        try:
+            image = cv2.imread(self.image_path, cv2.IMREAD_GRAYSCALE)
+
+            return image
+        except Exception as e:
+            print(f"Error in read_image: {e}")
+            return None
+
+
+    def set_image(self, label):
+        image = self.read_image()
+
+        if image is not None:
+            try:
+                q_image = self.convert_image_to_qimage(image)
+                q_pixmap = QGraphicsPixmapItem(QPixmap.fromImage(q_image))
+                scene = QGraphicsScene()
+                scene.addItem(q_pixmap)
+                label.setScene(scene)
+                label.fitInView(q_pixmap, Qt.KeepAspectRatio)
+
+            except Exception as e:
+                print(f"Error in set_image: {e}")
+
+
+    def convert_image_to_qimage(self, image):
+            try:
+                height, width = image.shape[:2]
+                bytes_per_line = 1 * width
+                q_image = QImage(image.data, width, height, bytes_per_line, QImage.Format_Grayscale8)
+
+                return q_image
+            except Exception as e:
+                print(f"Error in convert_image_to_qimage: {e}")
+                return None
+
+
+    def calculate_img_magnitude_phase(self, image):
+            try:
+                f_transform = np.fft.fft2(image)
+                f_transform_shifted = np.fft.fftshift(f_transform)
+                real_part = np.real(f_transform_shifted)
+                imaginary_part = np.imag(f_transform_shifted)
+                magnitude_spectrum =np.abs(f_transform_shifted)
+                phase_spectrum = np.angle(f_transform_shifted)
+                components=[real_part,imaginary_part,magnitude_spectrum,phase_spectrum]
+
+                return components
+            except Exception as e:
+                print(f"Error in calculate_img_magnitude_phase: {e}")
+                return None, None, None, None
+
+    def adjust_brightness(self, factor, label):
+        image = self.read_image()
+        try:
+                adjusted_image = self.adjust_brightness_opencv(image, factor)
+
+                if adjusted_image is not None:
+                    # Convert the adjusted image to QImage using OpenCV
+                    q_image = self.convert_image_to_qimage(adjusted_image)
+
+                    # Display the adjusted image in the label
+                    q_pixmap = QGraphicsPixmapItem(QPixmap.fromImage(q_image))
+                    scene = QGraphicsScene()
+                    scene.addItem(q_pixmap)
+                    label.setScene(scene)
+                    label.fitInView(q_pixmap, Qt.KeepAspectRatio)
+
+        except Exception as e:
+                print(f"Error adjusting brightness: {e}")
+
+    def adjust_brightness_opencv(self, image, factor):
+        try:
+            # Convert the image to float
+            image_float = image.astype('float32') / 255.0
+
+            # Adjust the brightness using OpenCV
+            adjusted_image = cv2.multiply(image_float, factor)
+
+            # Clip the values to stay within the valid range [0, 1]
+            adjusted_image = np.clip(adjusted_image, 0, 1)
+
+            # Convert back to uint8 format
+            adjusted_image = (adjusted_image * 255).astype('uint8')
+
+            return adjusted_image
+
+        except Exception as e:
+            print(f"Error in adjust_brightness_opencv: {e}")
+            return None
